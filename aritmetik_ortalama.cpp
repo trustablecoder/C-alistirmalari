9c75f4a8b0b8513658ad9391a18922af7605e593
trustablecoder <56368163+trustablecoder@users.noreply.github.com>
2020-08-16 17:56:08 +0300

    Create aritmetik_ortalama.cpp

---
+++ aritmetik_ortalama.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+	int n;
+	printf("Lutfen sayi miktarini giriniz: \n");
+	scanf ("%d", &n);
+	int i;
+	int k;
+	int toplam = 0;
+	int ortalama;
+	int a[n];
+	printf("Lutfen artimetik ortalamasi alinacak sayilari giriniz: ");
+	for (i=0; i<n; i+=1){
+		scanf("%d", &a[i]);
+	}
+	
+	for(k=0; k<n; k += 1){
+      	toplam = a[k]+ toplam;}
+
+	ortalama = toplam/n;
+	printf("Ortalama: %d",ortalama);
+}
