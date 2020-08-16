a4699c0f3fb582680546c154ebf834402caff71e
Simge <56368163+trustablecoder@users.noreply.github.com>
2020-08-16 19:16:22 +0300

    Update aritmetik_ortalama.c

--- aritmetik_ortalama.c
+++ aritmetik_ortalama.c
@@ -3,24 +3,28 @@ Kaç tane sayı girileceği ve aritmetik ortalaması alınacak olan sayıların 
 */
 #include <stdio.h>
 
-int main () {
-	int n;
-	printf("Lutfen sayi miktarini giriniz: \n");
-	scanf ("%d", &n);
-	int i;
-	int k;
-	int toplam = 0;
-	int ortalama;
-	int a[n];
-	printf("Lutfen artimetik ortalamasi alinacak sayilari giriniz: \n");
-	for (i=0; i<n; i+=1){
-		scanf("%d", &a[i]);
-	}
+	int main () 
+    {
+		
+		int n=0, toplam = 0, i, ortalama;
+		
+		printf("Lutfen sayi miktarini giriniz: \n");
+		scanf ("%d", &n);
+		
+        int a[n];
+		
+        printf("Lutfen artimetik ortalamasi alinacak sayilari giriniz: \n");
+		
+        for (i=0; i<n; i+=1)
+		{
+			scanf("%d", &a[i]);
+			toplam = a[i]+ toplam;
+		}
 	
-	for(k=0; k<n; k += 1){
-      	toplam = a[k]+ toplam;}
-
-	ortalama = toplam/n;
-	printf("Ortalama: %d",ortalama);
-	return 0;
-}
+		ortalama = toplam/n;
+		
+		printf("Ortalama: %d",ortalama);
+		
+        getch();
+        return 0;
+	}
