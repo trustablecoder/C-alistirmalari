f79212be9e096beda062c2693eb6fd66de780ac2
Simge <56368163+trustablecoder@users.noreply.github.com>
2021-02-17 14:59:45 +0300

    Update matris_satir_sutun_carpimi.c

--- matris_satir_sutun_carpimi.c
+++ matris_satir_sutun_carpimi.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 
 
-/* kullanıcıdan alınan matrisin satır ve sütunlarının çarpımını bulma */
+/* kullanÄ±cÄ±dan alÄ±nan matrisin satÄ±r ve sÃ¼tunlarÄ±nÄ±n Ã§arpÄ±mÄ±nÄ± bulma */
 
-void satir_carpim(int matris[][3]){
+int satir, sutun;
+
+
+void satir_carpim(int matris[satir][sutun]){
 	
-	int i,k,m; int satir_carpim[3];
+	int i,k,m; int satir_carpim[satir];
 	int carpim=1;
-	for(i=0;i<3;i++){
-		for(k=0;k<3;k++){
+	for(i=0;i<satir;i++){
+		for(k=0;k<sutun;k++){
 			carpim=carpim*matris[i][k];
 		}
 		satir_carpim[i]=carpim;
 		carpim=1;
 	}
 	
-	for(m=0;m<3;m++){
+	for(m=0;m<satir;m++){
 	printf("%d. satir carpimi: %d\n", m+1, satir_carpim[m]);
     }
     
     
 }
 
-void sutun_carpim(int matris[][3]){
+void sutun_carpim(int matris[satir][sutun]){
 	
-	int i,k,m; int sutun_carpim[3];
+	int i,k,m; int sutun_carpim[sutun];
 	int carpim=1;
-	for(k=0;k<3;k++){
-		for(i=0;i<3;i++){
+	for(k=0;k<satir;k++){
+		for(i=0;i<sutun;i++){
 			carpim=carpim*matris[i][k];
 		}
 		sutun_carpim[k]=carpim;
 		carpim=1;
 	}
 	
-	for(m=0;m<3;m++){
+	for(m=0;m<sutun;m++){
 	printf("%d. sutun carpimi: %d\n", m+1, sutun_carpim[m]);
     }
     
@@ -42,10 +45,12 @@ void sutun_carpim(int matris[][3]){
 }
 int main(){
 	int i,k; 	
-	int matris[3][3];
+	printf("satir ve sutun sayisini giriniz: ");
+	scanf("%d %d", &satir, &sutun);
+	int matris[satir][sutun];
 	printf("Matrisi giriniz: ");
-	for(i=0;i<3;i++){
-		for(k=0;k<3;k++){
+	for(i=0;i<satir;i++){
+		for(k=0;k<sutun;k++){
 			scanf("%d", &matris[i][k]);
 		}
 	}
