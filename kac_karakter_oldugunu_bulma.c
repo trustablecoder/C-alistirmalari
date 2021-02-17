49f833b2a80988e7df5630e40a10d4a8f2890864
Simge <56368163+trustablecoder@users.noreply.github.com>
2021-02-17 12:03:14 +0300

    Add files via upload

---
+++ kac_karakter_oldugunu_bulma.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int harfSay( char metin[]){
+
+/*
+
+Soru-> Dizide kaç karakter bulunduðunu sayan algoritmayý yazýnýz(dizide birden fazla olan karakterler bir kere sayýlýr.)
+
+*/
+
+int  toplam=0,i,k;
+int uzunluk = strlen(metin);
+for(i=0;i<uzunluk;i++)
+   {
+		for(k=0;k<i;k++)
+		{
+			if (metin[i]==metin[k])
+			{	
+				toplam = toplam +1;
+				break;
+			}
+		
+		}
+	}
+	
+return toplam;
+
+}
+
+
+int main(){
+
+	char metin[]= "cprogramlamaalistirmasi";
+
+	int sonuc = harfSay(metin);
+	printf("%d", strlen(metin)-sonuc);
+	
+
+	return 0;
+}
